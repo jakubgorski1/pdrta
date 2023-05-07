37650af6db0867d669e515e7909cb2ead600cd5b
jakubgorski1 <122107173+jakubgorski1@users.noreply.github.com>
2023-05-07 23:03:06 +0200

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3.11.3-slim-buster
+
+FROM python:3.10-slim-bullseye
 
 WORKDIR /app
 
--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from flask import request
 from flask import jsonify
 import numpy as np
 
-import random
-
 class Perceptron():
     
     def __init__(self, eta=0.01, n_iter=10):
@@ -16,20 +14,15 @@ class Perceptron():
         self.n_iter = n_iter
     
     def fit(self, X, y):
-        # self.w_ = np.zeros(1+X.shape[1])
-        
-        self.w_ = [random.uniform(-1.0, 1.0) for _ in range(1+X.shape[1])] 
+        self.w_ = np.zeros(1+X.shape[1])
         self.errors_ = []
         
         for _ in range(self.n_iter):
             errors = 0
             for xi, target in zip(X,y):
-                #print(xi, target)
                 update = self.eta*(target-self.predict(xi))
-                #print(update)
                 self.w_[1:] += update*xi
                 self.w_[0] += update
-                #print(self.w_)
                 errors += int(update != 0.0)
             self.errors_.append(errors)
         return self
@@ -49,27 +42,15 @@ def get_prediction():
 
     # sepal length
     sepal_length = float(request.args.get('sl'))
-    # sepal width
-    #sepal_width = float(request.args.get('sw'))
-    # petal length
     petal_length = float(request.args.get('pl'))
-    # petal width
-    #petal_width = float(request.args.get('pw'))
-
-    # The features of the observation to predict
-    #features = [sepal_length,
-    #            sepal_width,
-    #            petal_length,
-    #           petal_width]
     
     features = [sepal_length,
                 petal_length]
-    
-    print(features)
+
     # Load pickled model file
     with open('model.pkl',"rb") as picklefile:
         model = pickle.load(picklefile)
-    print(model)
+        
     # Predict the class using the model
     predicted_class = int(model.predict(features))
     
@@ -77,4 +58,4 @@ def get_prediction():
     return jsonify(features=features, predicted_class=predicted_class)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
